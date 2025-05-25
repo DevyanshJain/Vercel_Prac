a38ec0d3fc51bde5f57c34b80d6d220651d68d6c
Devyansh Jain <128241874+DevyanshJain@users.noreply.github.com>
2025-05-25 13:50:36 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,24 +1,17 @@
 import json
 
 def handler(request, response):
-    # Enable CORS
     response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
 
     if request.method != "GET":
         return response.status(405).send("Only GET allowed.")
 
     names = request.query.getlist("name")
-    result = []
 
     with open("marks.json") as f:
         data = json.load(f)
-    
-    # Convert list of dicts to a dict for faster lookup
-    data_dict = {item["name"]: item["marks"] for item in data}
 
-    for name in names:
-        result.append(data_dict.get(name, None))  # None if name not found
+    data_dict = {d["name"]: d["marks"] for d in data}
+    result = [data_dict.get(n, None) for n in names]
 
     return response.json({"marks": result})
